5b208041a307f2423e3e36bc33c99e8bee62ce9d
Rodrigo Gil <rodrigo2122sil@gmail.com>
2024-10-09 09:00:16 -0300

    function register collaborator

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 
 
+#function that shows all availables options
 def view_options():
     print('Hello, what are we going to do?\n'
           '1 - Register collaborator\n'
@@ -7,3 +8,15 @@ def view_options():
           '3 - Create work schadule\n'
           '4 - Edit registration'
           )
+    
+#function that register collaborators  
+def register_collaborator(collaborators):
+    collaborator = {}
+    collaborator ['name'] = input("Enter the collaborator's name: ")
+    collaborator ['time'] = input("Enter the collaborator's time: ")
+    collaborator ['scale'] = input('Enter the scale (6x1 or 5x2): ')
+    collaborators.append(collaborator)
+    
+    print(f'Collaborator {collaborator['name']} successfully registered!')
+
+    
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-from functions import view_options
+from functions import view_options, register_collaborator
+
+list_collaborators = list()
 
 view_options()
+
